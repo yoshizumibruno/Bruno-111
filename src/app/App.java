d1a1b7b8bba82a61820ca34f101135eb0652915e
41508971 <41508971@087915-P33S104.macklabs.br>
2017-11-01 09:58:21 -0200

    Adicionar variavel X

--- src/app/App.java
+++ src/app/App.java
@@ -15,7 +15,9 @@ public class App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      int x=10;
+      System.out.println("valor x = " + x);
+      int y=0;
     }
     
 }
